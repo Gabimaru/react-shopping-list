c3d378a7bbe4a61a3eccca9ca70653f8417002f0
Gabimaru <aashrader@yahoo.fr>
2020-09-09 15:10:03 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+import React from "react"
+import ShoppingApp from "./components/ShoppingApp.js"
+
+function App() {
+  return (
+    <div className="container my-3">
+      <header className="App-header">
+        <h1>Mon titre</h1>
+      </header>
+      <ShoppingApp />
+    </div>
+  )
+}
+
+export default App
